bbdcdf7dd8e5c91c6621b8e3989e86364b864f43
luke-h1 <luke.howsam@yahoo.com>
2020-11-19 22:07:46 +0000

    init commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimaryButton from './components/Buttons';
+
+const App = () => <h1>Hello World</h1>
+
+ReactDOM.render(<App />, document.querySelector('#root'));
